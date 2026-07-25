291c6c3916ce20943d3f779d8603411a580f33bb
Ravi mohanty <ravimohanty93@gmail.com>
2026-07-26 02:11:46 +0530

    dsa with swap the question

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+1  // concate the array ----> 
+
+// num = [1,2,1];
+// var getConcatenation = function(nums) {
+//    let n = num.length;
+//    let ans = new Array(n*2);
+//    for(let i=0;i<ans.length;i++){
+//      ans[i] = num[i%n];
+//    }
+//    return ans;
+
+// };
+
+
+// resverse an arrayy --------------------------------------------------- using temp extra variable
+// const A = [1,2,3,4,5,6]
+// function reverse() {
+//   const len = A.length;
+//   let i =0 
+//   let j = len-1;
+//   while(i < j) {
+//     let temp = A[i]
+//    A[i] = A[j]
+//    A[j] = temp
+//    i++, j--;
+//   }
+//   return A;
+// }
+
+
+
+// console.log( reverse());
---
+++ readme.md
@@ -0,0 +1,8 @@
+1
+
+Whenever you see a pattern like:
+
+"repeat/tile an array"
+"circular buffer / circular array"
+"wrap around to the beginning after reaching the end"
+indices that need to "loop back"
